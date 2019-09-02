109e7c38243cc86f6bc627beac8591d99791413f
Pumbik <lepiks@ukr.net>
2019-09-02 14:06:03 +0300

    В рамках задачи 435 изменил два запроса Coordinator_Button_Prozvon и Coordinator_Button_Rozyasneno

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DB_Koordynatory/Coordinator_Button_Prozvon/Coordinator_Button_Prozvon.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DB_Koordynatory/Coordinator_Button_Prozvon/Coordinator_Button_Prozvon.sql
@@ -3,7 +3,8 @@
   declare @t nvarchar(max)=N'
 
   update [CRM_1551_Analitics].[dbo].[Assignments]
-  set assignment_state_id=3
+  set
+   assignment_state_id=3
   ,[AssignmentResultsId]=4 
   ,[AssignmentResolutionsId]=4
   ,edit_date = GETUTCDATE()
@@ -13,15 +14,24 @@
   N'
   
   update [CRM_1551_Analitics].[dbo].[AssignmentConsiderations]
-  set [assignment_result_id]=4 
+  set 
+   [assignment_result_id]=4 
   ,[assignment_resolution_id]=4 
   ,edit_date = GETUTCDATE()
   ,user_edit_id = N'''+ @user + N'''
---   where assignment_id=in ( '+ @ids+N')
-  where Id in ( select current_assignment_consideration_id from Assignments where Id in ('+@Ids+N') )
+  where Id in ( select current_assignment_consideration_id from dbo.Assignments where Id in ('+@Ids+N') )
+
+  update dbo.AssignmentRevisions
+    set
+     assignment_resolution_id = 4
+    ,control_result_id = NULL
+    ,control_type_id = 1
+    ,edit_date = GETUTCDATE()
+    ,user_edit_id = N'''+ @user + N'''
+  where assignment_consideration_іd in (SELECT current_assignment_consideration_id FROM dbo.Assignments WHERE Id IN ('+@Ids+N') )
+
   '
-  
-  
   
   --select @t
   exec (@t)
+
