3bdfd38784775510e461531cf0f0a6e5baddff88
MarkusEfr <markus.efr@gmail.com>
2019-09-16 16:28:38 +0300

    Добавил поле "Опис" в запросы  на список тип питання карточки оператора

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/FormAppeals/List/List_QuestionTypes_SelectRows_Search/List_QuestionTypes_SelectRows_Search.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/FormAppeals/List/List_QuestionTypes_SelectRows_Search/List_QuestionTypes_SelectRows_Search.sql
@@ -3,21 +3,26 @@ with [Organisations] as (
        t1.[Id]
       ,t1.[Name]
       ,t1.[ParentId]
+	  ,t1.comments
       ,CONVERT(nvarchar(max), isnull(t1.[Name],N'')) as [Path]
-    from (select [Id] ,[name] as [Name] ,[question_type_id] as [ParentId] from [dbo].[QuestionTypes] where active = 1) as t1
+    from (select [Id] ,[name] as [Name] ,[question_type_id] as [ParentId], comments
+	      from [dbo].[QuestionTypes] where active = 1) as t1
     where [ParentId] is NULL
     union all
     select
        t1.[Id]
       ,t1.[Name]
       ,t1.[ParentId]
+	  ,t1.comments
       ,CONVERT(nvarchar(max), isnull([Path],N'') + N'\' + isnull(t1.[Name],N'')) as [Path]
-    from (select [Id] ,[name] as [Name] ,[question_type_id] as [ParentId] from [dbo].[QuestionTypes] where active = 1) as t1
+    from (select [Id] ,[name] as [Name] ,[question_type_id] as [ParentId], comments
+	      from [dbo].[QuestionTypes] where active = 1) as t1
     join [Organisations] on [Organisations].[Id] = t1.[ParentId]
 )
 select
        [Id]
       ,[Name]
+	    ,comments
       ,[Path]
 from [Organisations]
 where #filter_columns#
