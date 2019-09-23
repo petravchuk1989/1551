18f702f09a44575548f615dfb9de53fc0edbe105
Pumbik <lepiks@ukr.net>
2019-09-23 16:44:51 +0300

    Добавил секцию, форму и запросы для них - Gorodok_glodal

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Gorodok_global/Gorodok_global_SelectRows/Gorodok_global_SelectRows.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Gorodok_global/Gorodok_global_SelectRows/Gorodok_global_SelectRows.sql
@@ -1,11 +1,12 @@
-SELECT [gg].claim_number as [Id] -- № заходу
-	  ,gg.claim_number as event_type_name -- Тип заходу
-	  ,gg.claims_type as event_class_name -- Клас заходу
+SELECT [gg].claim_number as [Id] -- № заходу +
+	  ,gg.claims_type as event_type_name -- Тип заходу
+	--   ,gg.claims_type as event_class_name -- Клас заходу +
 	  ,gg.object_name -- Об'єкт
 	  ,gg.executor as executor_name -- Виконавець
 	  ,gg.registration_date  as start_date -- Дата початку
 	  ,gg.fact_finish_date as real_end_date -- Факт.завершення
   FROM [CRM_1551_GORODOK_Integrartion].[dbo].[Lokal_copy_gorodok_global] as gg
--- 	where #filter_columns#
---      #sort_columns#
---  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+
+	where #filter_columns#
+     #sort_columns#
+ offset @pageOffsetRows rows fetch next @pageLimitRows rows only
