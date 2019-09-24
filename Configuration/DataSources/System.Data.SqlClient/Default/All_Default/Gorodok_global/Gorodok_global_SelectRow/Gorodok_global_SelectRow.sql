2e8cf76d97202142cc7aab77e72a99f4841471d7
Pumbik <lepiks@ukr.net>
2019-09-24 16:15:12 +0300

    Доб. 4 детали к к.Gorodok_global и написал все связанные запросы к ним

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Gorodok_global/Gorodok_global_SelectRow/Gorodok_global_SelectRow.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Gorodok_global/Gorodok_global_SelectRow/Gorodok_global_SelectRow.sql
@@ -3,10 +3,14 @@ SELECT [gg].claim_number AS [Id] -- № заходу +
 	--   ,gg.claims_type as event_class_name -- Клас заходу +
 	  , gg.object_name -- Об'єкт
 	  , gg.executor AS executor_name -- Виконавець
-	  , gg.registration_date -- Дата початку
-	  , gg.fact_finish_date AS real_end_date-- Факт.завершення
+	--   , gg.registration_date -- Дата початку
+	  , CONVERT(datetime, CONVERT(datetimeoffset(4),gg.registration_date) AT TIME ZONE 'Pacific SA Standard Time') AS registration_date
+	--   , gg.fact_finish_date AS real_end_date-- Факт.завершення
       ,gg.[executor_comment]
-      ,gg.plan_finish_date
+	  ,gg.[content]
+    --   ,gg.plan_finish_date
+      ,CONVERT(datetime, CONVERT(datetimeoffset(4),gg.plan_finish_date) AT TIME ZONE 'Pacific SA Standard Time') AS plan_finish_date
+	  ,CONVERT(datetime, CONVERT(datetimeoffset(4),gg.fact_finish_date) AT TIME ZONE 'Pacific SA Standard Time') as real_end_date
 FROM [CRM_1551_GORODOK_Integrartion].[dbo].[Lokal_copy_gorodok_global] AS gg
 
 WHERE claim_number = @Id
---
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Gorodok_global/cx_Event_Objects_Gl/cx_Event_Objects_Gl.sql
@@ -0,0 +1,130 @@
+  --declare @event_id int =8;
+
+SELECT
+   o.Id
+	, [Districts].name [district_name]
+	, [ObjectTypes].name obj_type_name
+	, o.name [object_name]
+FROM [CRM_1551_GORODOK_Integrartion].[dbo].[Lokal_copy_gorodok_global] AS gl
+
+   JOIN [CRM_1551_GORODOK_Integrartion].[dbo].[AllObjectInClaim] AS oc ON oc.claims_number_id = gl.claim_number
+   JOIN [CRM_1551_GORODOK_Integrartion].[dbo].[Gorodok_1551_houses] gh ON gh.gorodok_houses_id = oc.object_id
+   JOIN [CRM_1551_Analitics].[dbo].[Objects] AS o ON o.Id = gh.[1551_houses_id]
+   JOIN [CRM_1551_Analitics].[dbo].[ObjectTypes] ON o.object_type_id=[ObjectTypes].Id
+   JOIN [CRM_1551_Analitics].[dbo].[Buildings] ON o.builbing_id=[Buildings].Id
+   JOIN [CRM_1551_Analitics].[dbo].[Districts] ON [Buildings].district_id=[Districts].Id
+WHERE gl.claim_number = @Id
+
+  and #filter_columns#
+     #sort_columns#
+  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+
+
+
+
+  /*
+  select [ObjectsInObject].[object_id] Id, [Districts].name [district_name] , [ObjectTypes].name obj_type_name,
+  [Objects].name [object_name]
+ -- , [Objects].Id, [ObjectsInObject].Id, [ObjectsInObject].main_object_id
+  from [CRM_1551_Analitics].[dbo].[EventObjects]
+  --inner join [CRM_1551_Analitics].[dbo].[Objects] on [EventObjects].[object_id]=[Objects].Id
+  inner join [CRM_1551_Analitics].[dbo].[ObjectsInObject] on [EventObjects].[object_id]=[ObjectsInObject].main_object_id
+  left join [CRM_1551_Analitics].[dbo].[Objects] on [ObjectsInObject].[object_id]=[Objects].Id
+  left join [CRM_1551_Analitics].[dbo].[ObjectTypes] on [Objects].object_type_id=[ObjectTypes].Id
+  left join [CRM_1551_Analitics].[dbo].[Buildings] on [Objects].builbing_id=[Buildings].Id
+  left join [CRM_1551_Analitics].[dbo].[Districts] on [Buildings].district_id=[Districts].Id
+  where [EventObjects].event_id=@event_id
+and #filter_columns#
+     #sort_columns#
+  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+ */
+
+-- SELECT 
+-- 	Id
+-- 	,district_name
+-- 	,obj_type_name
+-- 	,[object_name]
+-- 	from(
+-- select EventObjects.Id
+-- 	  ,Districts.name as district_name
+-- 	  ,ObjectTypes.name as obj_type_name
+-- 	  ,IIF([Objects].name is null, concat(StreetTypes.shortname,' ', Streets.name, ' ', Buildings.number,Buildings.letter),
+-- 	   IIF(Buildings.street_id is null,[Objects].name, concat(StreetTypes.shortname,' ',Streets.name, ' ', Buildings.number,Buildings.letter,' ( ',[Objects].name,' )'))
+-- 		 )as [object_name]
+
+-- 	from EventObjects
+-- 		left join [Events] on [Events].Id = EventObjects.event_id
+-- 		left join [Objects] on [Objects].Id = EventObjects.[object_id]
+-- 		left join Buildings on Buildings.Id = [Objects].builbing_id
+-- 		left join Streets on Streets.Id = Buildings.street_id
+-- 		left join StreetTypes on StreetTypes.Id = Streets.street_type_id
+-- 		left join Districts on Districts.Id = Buildings.district_id
+-- 		left join ObjectTypes on ObjectTypes.Id = [Objects].object_type_id
+-- 	where EventObjects.event_id = @event_id
+-- 		and EventObjects.Id <> (select top 1 Id from EventObjects where event_id = @event_id)
+
+-- union all
+
+-- select 0 as id
+-- 	  ,Districts.name as district_name
+-- 	  ,ObjectTypes.name as obj_type_name
+-- 	  ,IIF([Objects].name is null, concat(StreetTypes.shortname,' ',Streets.name, ' ', Buildings.number,Buildings.letter),
+-- 	   IIF(Buildings.street_id is null,[Objects].name, concat(StreetTypes.shortname,' ',Streets.name, ' ', Buildings.number,Buildings.letter,' ( ',[Objects].name,' )'))
+-- 		 )as [object_name]
+-- 	from ObjectsInObject
+-- 		left join [Objects] on [Objects].Id =  ObjectsInObject.[object_id] 
+-- 		left join [Objects] obj on obj.Id = ObjectsInObject.main_object_id 
+-- 		left join Buildings on Buildings.Id = [Objects].builbing_id
+-- 		left join Streets on Streets.Id = Buildings.street_id
+-- 		left join StreetTypes on StreetTypes.Id = Streets.street_type_id
+-- 		left join Districts on Districts.Id = Buildings.district_id
+-- 		left join ObjectTypes on ObjectTypes.Id = [Objects].object_type_id
+-- 	where ObjectsInObject.main_object_id = ( select top 1 object_id from EventObjects where event_id = @event_id )
+
+-- union all
+
+-- select 0 as id
+-- 	  ,Districts.name as district_name
+-- 	  ,ObjectTypes.name as obj_type_name
+-- 	  ,IIF([Objects].name is null, concat(Streets.name, ' ', Buildings.number,Buildings.letter),
+-- 	   IIF(Buildings.street_id is null,[Objects].name, concat(Streets.name, ' ', Buildings.number,Buildings.letter,' ( ',[Objects].name,' )'))
+-- 		 )as [object_name]
+-- 	from ObjectsInObject
+-- 		left join [Objects] on [Objects].Id = ObjectsInObject.main_object_id   
+-- 		left join [Objects] obj on obj.Id = ObjectsInObject.[object_id]
+-- 		left join Buildings on Buildings.Id = [Objects].builbing_id
+-- 		left join Streets on Streets.Id = Buildings.street_id
+-- 		left join StreetTypes on StreetTypes.Id = Streets.street_type_id
+-- 		left join Districts on Districts.Id = Buildings.district_id
+-- 		left join ObjectTypes on ObjectTypes.Id = [Objects].object_type_id
+-- 	where ObjectsInObject.[object_id] = ( select top 1 object_id from EventObjects where event_id = @event_id )
+-- 	) as tab
+-- 	where #filter_columns#
+--     #sort_columns#
+--  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+
+-- /*
+-- select	EventObjects.Id
+-- 	  ,Districts.name as district_name
+-- 	  ,ObjectTypes.name as obj_type_name
+-- 	  ,IIF(Objects.name is null, 
+-- 		concat(Streets.name, ' ', Buildings.number,Buildings.letter),
+-- 		IIF(Buildings.street_id is null,
+-- 			Objects.name
+-- 			,concat(Streets.name, ' ', Buildings.number,Buildings.letter,' ( ',Objects.name,' )')
+-- 		)
+-- 		 )as object_name
+
+-- 	from EventObjects
+-- 		left join Events on Events.Id = EventObjects.event_id
+-- 		left join Objects on Objects.Id = EventObjects.object_id
+-- 		left join Buildings on Buildings.Id = Objects.builbing_id
+-- 		left join Streets on Streets.Id = Buildings.street_id
+-- 		left join StreetTypes on StreetTypes.Id = Streets.street_type_id
+-- 		left join Districts on Districts.Id = Buildings.district_id
+-- 		left join ObjectTypes on ObjectTypes.Id = Objects.object_type_id
+-- 	where EventObjects.event_id = @event_id
+-- 	and #filter_columns#
+--     #sort_columns#
+--  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+--  */
