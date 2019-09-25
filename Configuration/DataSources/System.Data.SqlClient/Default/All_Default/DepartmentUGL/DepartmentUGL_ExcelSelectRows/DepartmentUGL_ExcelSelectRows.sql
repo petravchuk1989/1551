aa7e74ba9cad1cc5a1dbfc028dab12b5ed103f9e
artemkov1994 <artem_kov@email.ua>
2019-09-25 11:16:40 +0300

    зарос_на_третью_кнопку_departmentUGL_ExcelButton3_изменил_отображение_вывода_таблицы

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
@@ -3,8 +3,9 @@ declare @dateTo date;
 declare @user_id nvarchar(128);
 declare @is_worked bit;
 */
-select [№ звернення] [Вихідний номер УГЛ], [Створено] [Дата реєстрації], [Заявник], [Адреса], [Зміст]
+select [№ звернення] [EnterNumber], [Створено] [RegistrationDate], [Заявник] [Applicant], [Адреса] [Address], [Зміст] [Content], [Питання] QuestionNumber
 from [Звернення УГЛ]
-where convert(date, [Створено]) between convert(date, @dateFrom) and convert(date, @dateTo)
-and [Опрацював]=@user_id
-and [Опрацьовано]=@is_worked
+where convert(date, [Створено]) between @dateFrom and @dateTo
+and #filter_columns# /*[Опрацював]=@user_id*/
+and ([Опрацьовано]= case when @is_worked is null then 'true' else @is_worked end or 
+[Опрацьовано]= case when @is_worked is null then 'false' else @is_worked end)
