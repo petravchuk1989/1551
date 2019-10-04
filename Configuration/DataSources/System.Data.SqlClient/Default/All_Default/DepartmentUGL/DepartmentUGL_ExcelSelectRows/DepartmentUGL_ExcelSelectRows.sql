629ee78824e4154fa13a94babac92fe0f389aae7
Yehor <egorkatornado@gmail.com>
2019-10-04 12:51:57 +0300

    Добавил запросы на ДБ и секции. ДБ работает корректно. Ошибка только в загрузочных файлах

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/DepartmentUGL/DepartmentUGL_ExcelSelectRows/DepartmentUGL_ExcelSelectRows.sql
@@ -3,9 +3,24 @@ declare @dateTo date;
 declare @user_id nvarchar(128);
 declare @is_worked bit;
 */
-select [№ звернення] [EnterNumber], [Створено] [RegistrationDate], [Заявник] [Applicant], [Адреса] [Address], [Зміст] [Content], [Питання] QuestionNumber
+if @is_worked is not null
+begin
+
+select Id, [№ звернення] [EnterNumber], [Створено] [RegistrationDate], [Заявник] [Applicant], [Адреса] [Address], [Зміст] [Content], [Питання] QuestionNumber
 from [Звернення УГЛ]
 where convert(date, [Створено]) between @dateFrom and @dateTo
 and #filter_columns# /*[Опрацював]=@user_id*/
-and ([Опрацьовано]= case when @is_worked is null then 'true' else @is_worked end or 
-[Опрацьовано]= case when @is_worked is null then 'false' else @is_worked end)
+and [Опрацьовано]= @is_worked 
+
+-- #sort_columns#
+--  offset @pageOffsetRows rows fetch next @pageLimitRows rows only
+
+end
+
+if @is_worked is null
+begin
+select Id, [№ звернення] [EnterNumber], [Створено] [RegistrationDate], [Заявник] [Applicant], [Адреса] [Address], [Зміст] [Content], [Питання] QuestionNumber
+from [Звернення УГЛ]
+where convert(date, [Створено]) between @dateFrom and @dateTo
+and #filter_columns# /*[Опрацював]=@user_id*/
+end
