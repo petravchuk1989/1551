9cb40b8beb8856216170578c7a1fdeb3ff8093cf
artemkov1994 <artem_kov@email.ua>
2019-10-29 11:50:32 +0200

    appeal_insert_изменил_порядок_присвоения_регистрационного_номера

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/FormAppeals/Appeals_Insert/Appeals_Insert.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals/FormAppeals/Appeals_Insert/Appeals_Insert.sql
@@ -2,7 +2,7 @@ declare @output table (Id int)
 
 INSERT INTO [dbo].[Appeals]
            ([registration_date]
-           --,[registration_number]
+           ,[registration_number]
            ,[receipt_source_id]
            ,[phone_number]
            ,[receipt_date]
@@ -14,6 +14,7 @@ output [inserted].[Id] into @output (Id)
      VALUES
            (getutcdate() --@registration_date
            --,@registration_number
+		   ,concat( SUBSTRING ( rtrim(YEAR(getdate())),4,1),'-',(select count(Id)+1 from Appeals where year(Appeals.registration_date) = year(getdate())) ) 
            ,@receipt_source_id
            ,@phone_number
            ,getutcdate() -- @receipt_date
@@ -27,7 +28,10 @@ declare @app_id int
 set @app_id = (select top 1 Id from @output)
 
 -- update [dbo].[Appeals] set registration_number =  concat( YEAR(getdate()),'-',MONTH(getdate()),'/',@app_id  ) where Id =  @app_id
-update [dbo].[Appeals] set registration_number =  concat( SUBSTRING ( rtrim(YEAR(getdate())),4,1),'-',(select count(Id) from Appeals where year(Appeals.registration_date) = year(getdate())) ) where Id =  @app_id
-
+/*
+update [dbo].[Appeals] 
+set registration_number =  concat( SUBSTRING ( rtrim(YEAR(getdate())),4,1),'-',(select count(Id) from Appeals where year(Appeals.registration_date) = year(getdate())) ) 
+where Id =  @app_id
+*/
 select @app_id as [Id]
 return;
