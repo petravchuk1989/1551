57c84ffba61028b94a265809773b99185bc02e26
MarkusEfr <markus.efr@gmail.com>
2019-11-07 16:57:54 +0200

    Доработки по УГЛ

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals_UGL/Appeals_UGL/AppealUGL_Info.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/Appeals_UGL/Appeals_UGL/AppealUGL_Info.sql
@@ -1,36 +1,38 @@
--- declare @Id int = 15379
-select top 1
+-- declare @Id int = 5398611
+select 
 [№ звернення] as incomNum, 
-
 IIF(
-charindex(',', a.Телефон) > 0, 
-(substring(a.Телефон, 1, 10)),
-a.Телефон 
+charindex(',', Телефон) > 0, 
+(substring(Телефон, 1, 10)),
+Телефон 
 )  as phone,
-a.Телефон as full_phone,
-convert(varchar, a.[Дата завантаження],(120)) as incomDate,
-a.Заявник as Applicant_PIB,
-a.Зміст as Question_Content, 
-a.Заявник +
+Телефон as full_phone,
+convert(varchar, [Дата завантаження],(120)) as incomDate,
+Заявник as Applicant_PIB,
+Зміст as Question_Content, 
+Заявник +
   + CHAR(13) +
- case when a.Адреса is not null then 
- 'Адреса: ' + a.Адреса else '' end
+ case when Адреса is not null then 
+ 'Адреса: ' + Адреса else '' end
   + CHAR(13) + 
-  case when a.[E-mail] is not null then 
-  a.[E-mail] else '' end 
-  + case when a.[Соціальний стан] is not null and a.[E-mail] is not null then 
-   ', Соц. стан: ' + a.[Соціальний стан] 
+  case when [E-mail] is not null then 
+  [E-mail] else '' end 
+  + case when [Соціальний стан] is not null and [E-mail] is not null then 
+   ', Соц. стан: ' + [Соціальний стан] 
          when a.[Соціальний стан] is not null and a.[E-mail] is null then 
-		 'Соц. стан: ' +a.[Соціальний стан] else '' end +
-   case when a.Категорія is not null and a.[Соціальний стан] is not null then 
-   ( ', ' + 'Категорія: ' + a.Категорія) 
-   when a.Категорія is not null and a.[Соціальний стан] is null then
-   ('Категорія: ' + a.Категорія) else '' end + 
-   case when a.[Дата народження] is not null and a.Категорія is not null then 
-  ', ' + cast(a.[Дата народження] as varchar) 
-  when a.[Дата народження] is not null and a.Категорія is null then
-  cast(a.[Дата народження] as varchar)  else '' end
- as ApplicantUGL
+		 'Соц. стан: ' + [Соціальний стан] else '' end +
+   case when a.Категорія is not null and [Соціальний стан] is not null then 
+   ( ', ' + 'Категорія: ' + Категорія) 
+   when Категорія is not null and [Соціальний стан] is null then
+   ('Категорія: ' + Категорія) else '' end + 
+   case when [Дата народження] is not null and Категорія is not null then 
+  ', ' + cast([Дата народження] as varchar) 
+  when [Дата народження] is not null and a.Категорія is null then
+  cast([Дата народження] as varchar)  else '' end
+ as ApplicantUGL,
+ a.Id as uglId,
+ appeal.registration_number as appealNum
 
 from dbo.[Звернення УГЛ] a
-where a.Id = @Id 
+join Appeals appeal on appeal.Id = a.Appeals_id
+where Appeals_id = @Id 
