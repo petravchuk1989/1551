25a3e4bf2bd58f4b2d3a399d1bd02f2fd68f8266
Yehor <egorkatornado@gmail.com>
2019-12-10 15:32:58 +0200

    Changed the request for the correct operation of the request

--- Configuration/DataSources/System.Data.SqlClient/Default/All_Default/table_poshuk/timeliness/timeliness.sql
+++ Configuration/DataSources/System.Data.SqlClient/Default/All_Default/table_poshuk/timeliness/timeliness.sql
@@ -1,4 +1,11 @@
+SELECT Id, Name FROM (
 
-select 1 Id, N'Вчасно' Name
-union all
-select 2 Id, N'Не вчасно' Name
+    select 1 Id, N'Вчасно' Name
+    union all
+    select 2 Id, N'Не вчасно' Name
+) t
+
+where 
+  #filter_columns#
+  #sort_columns#
+ offset @pageOffsetRows rows fetch next @pageLimitRows rows only
